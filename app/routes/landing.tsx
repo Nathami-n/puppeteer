88a62cf32dd3b6d5a8ba8d8b76b8acd78ae0fd7d
Nathami <georgenathan010@gmail.com>
2025-08-10 01:47:07 +0300

    add: the user dropdown menu

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -1,20 +1,21 @@
+import UserDropdown from "~/components/custom/user-dropdown";
 import {
-  HomePageHeader,
-  Hero,
-  Company,
   Bento,
-  Quote,
-  PricingSection,
-  Testimonials,
-  FooterSection,
+  Company,
   Faqs,
-  // CombinedFeaturedSection 
+  FooterSection,
+  Hero,
+  HomePageHeader,
+  PricingSection,
+  Quote,
+  Testimonials,
 } from "~/features/landing";
-
 
 export default function LandingPage() {
   return (
+
     <main className="max-w-7xl mx-auto border-x border-border relative">
+      <UserDropdown />
       <div className="block w-px h-full  border-border border-l absolute top-0 left-2 z-10" />
       <div className="block w-px h-full  border-border border-r  absolute top-0 right-2 z-10"></div>
       <HomePageHeader />
