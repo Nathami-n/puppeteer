2ccd682af28da15af3732fc06b84d2e1073d0a80
Nathami <georgenathan010@gmail.com>
2025-08-14 01:32:35 +0300

    feat: inpu fields

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,8 +1,15 @@
-import * as React from "react"
+import type * as React from "react";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({
+  className,
+  type,
+  error,
+  ...props
+}: React.PropsWithChildren<
+  React.ComponentProps<"input"> & { error?: boolean }
+>) {
   return (
     <input
       type={type}
@@ -11,11 +18,46 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
+        {
+          "border-destructive aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40":
+            error,
+        }
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Input }
+function InputError({
+  className,
+  children,
+  ...props
+}: React.PropsWithChildren<React.ComponentProps<"div">>) {
+  return (
+    <div
+      className={cn(
+        "mt-0 w-full text-left text-destructive text-sm",
+        className
+      )}
+      {...props}
+      role="alert"
+    >
+      {children}
+    </div>
+  );
+}
+
+function InputField({
+  children,
+  className,
+  ...rest
+}: React.PropsWithChildren<React.ComponentProps<"div">>) {
+  return (
+    <div className={cn("flex w-full flex-col gap-1", className)} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export { Input, InputError, InputField };
--- app/features/auth/components/sign-up.tsx
+++ app/features/auth/components/sign-up.tsx
@@ -1,57 +1,43 @@
-import * as React from "react";
-import { href, Link } from "react-router";
+import { Form, href, Link } from "react-router";
 import { Logo } from "~/components/custom/logo";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
 import RandomUsers from "./random-users";
+import { Separator } from "~/components/ui/separator";
+import { Input, InputError, InputField } from "~/components/ui/input";
 
 export const SignUpComponent = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState("");
-
-  const validateEmail = (email: string) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  };
-
-  const handleSignIn = () => {
-    if (!email || !password) {
-      setError("Please enter both email and password.");
-      return;
-    }
-    if (!validateEmail(email)) {
-      setError("Please enter a valid email address.");
-      return;
-    }
-    setError("");
-    alert("Sign in successful! (Demo)");
-  };
-
   return (
-    <div>
+    <Form>
       <div className="relative z-10 w-full max-w-sm rounded-3xl  shadow-2xl p-8 flex flex-col items-center">
-        <div className="mb-2">
+        <div>
           <Logo
             className="text-primary"
             containerClassName="flex flex-col items-center"
             text="CrawliQ"
           />
         </div>
+        <h1 className="text-3xl md:text-4xl  font-medium tracking-tighter text-center mb-2">
+          Let's set you up!
+        </h1>
 
         <div className="flex flex-col w-full gap-4">
           <div className="w-full flex flex-col gap-3">
-            <Input />
-            <Input />
+            <div>
+              <InputField className="relative">
+                <Input />
+              </InputField>
+              <InputError>error</InputError>
+            </div>
           </div>
-          <hr className="opacity-10" />
+          <Separator />
           <div className="space-y-2">
             <Button
-              onClick={handleSignIn}
+              type="submit"
               className="w-full bg-primary hover:bg-primary/90 text-white cursor-pointer"
             >
-              Sign in
+              Sign up
             </Button>
-          
+
             <Button className="w-full flex items-center justify-center gap-2 bg-gradient-to-b from-[#232526] to-[#2d2e30] rounded-full px-5 py-3 font-medium text-white shadow hover:brightness-110 transition mb-2 text-sm">
               <img
                 src="https://www.svgrepo.com/show/475656/google-color.svg"
@@ -74,6 +60,6 @@ export const SignUpComponent = () => {
       <div className="mt-12 ">
         <RandomUsers />
       </div>
-    </div>
+    </Form>
   );
 };
