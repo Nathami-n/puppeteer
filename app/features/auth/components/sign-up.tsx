53048832e782e3624775f6597bb5a17c173330ff
Nathami <georgenathan010@gmail.com>
2025-08-14 12:31:01 +0300

    add: the show password and form refactor

--- app/features/auth/components/sign-up.tsx
+++ app/features/auth/components/sign-up.tsx
@@ -1,20 +1,37 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Eye, EyeClosed } from "@solar-icons/react/ssr";
 import { useState } from "react";
 import { Form, href, Link } from "react-router";
+import { useRemixForm } from "remix-hook-form";
 import { Logo } from "~/components/custom/logo";
 import { Button } from "~/components/ui/button";
 import { Input, InputError, InputField } from "~/components/ui/input";
 import { Separator } from "~/components/ui/separator";
+import { signUpSchema } from "../zod";
 import RandomUsers from "./random-users";
 
-
+export const resolver = zodResolver(signUpSchema);
 
 export const SignUpComponent = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useRemixForm({
+    resolver,
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onSubmit",
+  });
+
   return (
-    <Form>
+    <div className="flex flex-col">
       <div className="relative z-10 bg-white w-full max-w-sm rounded-3xl  shadow-2xl p-8 flex flex-col items-center">
         <div>
           <Logo
@@ -28,83 +45,101 @@ export const SignUpComponent = () => {
         </h1>
 
         <div className="flex flex-col w-full gap-4">
-          <div className="w-full flex flex-col gap-3">
-            <div>
-              <InputField className="relative">
-                <Input placeholder="Email" />
-              </InputField>
-              <InputError>error</InputError>
-            </div>
-            <div>
-              <InputField className="relative">
-                <Input placeholder="Password" />
-                {showPassword ? (
-                  <EyeClosed
-                    weight="LineDuotone"
-                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
-                    onClick={() => setShowPassword(!showPassword)}
-                  />
-                ) : (
-                  <Eye
-                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
-                    onClick={() => setShowPassword(!showPassword)}
-                    weight="LineDuotone"
-                  />
+          <Form onSubmit={handleSubmit} method="POST">
+            <div className="w-full flex flex-col gap-3">
+              <div>
+                <InputField className="relative">
+                  <Input placeholder="Email" {...register("email")} />
+                </InputField>
+                {errors.email && (
+                  <InputError>{errors.email.message}</InputError>
                 )}
-              </InputField>
-              <InputError>error</InputError>
-            </div>
-            <div>
-              <InputField className="relative">
-                <Input placeholder="Confirm password" />
-                {showConfirmPassword ? (
-                  <EyeClosed
-                    weight="LineDuotone"
-                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+              </div>
+              <div>
+                <InputField className="relative">
+                  <Input
+                    placeholder="Password"
+                    {...register("password")}
+                    type={showPassword ? "text" : "password"}
                   />
-                ) : (
-                  <Eye
-                    weight="LineDuotone"
-                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  />
+                  {showPassword ? (
+                    <EyeClosed
+                      weight="LineDuotone"
+                      className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                    />
+                  ) : (
+                    <Eye
+                      className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                      onClick={() => setShowPassword(!showPassword)}
+                      weight="LineDuotone"
+                    />
+                  )}
+                </InputField>
+                {errors.password && (
+                  <InputError>{errors.password.message}</InputError>
                 )}
-              </InputField>
-              <InputError>error</InputError>
+              </div>
+              <div>
+                <InputField className="relative">
+                  <Input
+                    placeholder="Confirm password"
+                    {...register("confirmPassword")}
+                    type={showConfirmPassword ? "text" : "password"}
+                  />
+                  {showConfirmPassword ? (
+                    <EyeClosed
+                      weight="LineDuotone"
+                      className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    />
+                  ) : (
+                    <Eye
+                      weight="LineDuotone"
+                      className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                    />
+                  )}
+                </InputField>
+                {errors.confirmPassword && (
+                  <InputError>{errors.confirmPassword.message}</InputError>
+                )}
+              </div>
             </div>
-          </div>
-          <Separator />
-          <div className="space-y-2">
+            <Separator className="my-2" />
             <Button
               type="submit"
               className="w-full bg-primary hover:bg-primary/90 text-white cursor-pointer"
             >
               Sign up
             </Button>
+          </Form>
 
-            <Button className="w-full flex items-center justify-center gap-2 bg-gradient-to-b from-[#232526] to-[#2d2e30] rounded-full px-5 py-3 font-medium text-white shadow hover:brightness-110 transition mb-2 text-sm">
-              <img
-                src="https://www.svgrepo.com/show/475656/google-color.svg"
-                alt="Google"
-                className="w-5 h-5"
-              />
-              Continue with Google
-            </Button>
-            <div className="w-full text-center mt-2">
-              <span className="text-xs text-muted-foreground">
-                Already have an account?{" "}
-                <Link to={href("/sign-up")} className="underline">
-                  Sign in!
-                </Link>
-              </span>
-            </div>
+          <Button className="gap-2 bg-gradient-to-b cursor-pointer from-[#232526] to-[#2d2e30] font-medium text-white shadow hover:brightness-110 transition text-sm">
+            <img
+              src="/icons/google-color.svg"
+              alt="Google"
+              className="size-5"
+            />
+            Continue with Google
+          </Button>
+          <div className="w-full text-center mt-2">
+            <span className="text-xs text-muted-foreground">
+              Already have an account?{" "}
+              <Link to={href("/sign-up")} className="underline">
+                Sign in!
+              </Link>
+            </span>
           </div>
         </div>
       </div>
       <div className="mt-12 ">
         <RandomUsers />
       </div>
-    </Form>
+    </div>
   );
 };
--- app/routes/auth/sign-up.tsx
+++ app/routes/auth/sign-up.tsx
@@ -1,5 +1,23 @@
-import { SignUpComponent } from "~/features/auth";
+import { getValidatedFormData } from "remix-hook-form";
+import {
+  resolver,
+  SignUpComponent,
+  type SignupUserPayload,
+} from "~/features/auth";
+import type { Route } from "./+types/sign-in";
 
+export const action = async ({ request }: Route.ActionArgs) => {
+  const {
+    errors,
+    data,
+    receivedValues: defaultValues,
+  } = await getValidatedFormData<SignupUserPayload>(request, resolver);
+  if (errors) {
+    return { errors, defaultValues };
+  }
+  console.log(data)
+  return data;
+};
 export default function SignUpPage() {
   return <SignUpComponent />;
 }
