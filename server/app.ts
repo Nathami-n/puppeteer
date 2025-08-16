42c5349fdde691c0d93148e2ade74cf94ae9e953
Nathami <georgenathan010@gmail.com>
2025-08-16 23:28:32 +0300

    feat: the frontend route definition

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import { createRequestHandler } from "@react-router/express";
 import express from "express";
 import { unstable_RouterContextProvider } from "react-router";
 import { sessionContext } from "~/context/session";
+import { auth } from "~/lib/auth.server";
 
 export const app = express();
 function getLoadContext() {
@@ -12,9 +13,22 @@ function getLoadContext() {
 	return context;
 }
 
+app.use("/testing", (req, res) => {
+	const session = auth.api.getSession({
+		headers: req.headers,
+	});
+  console.log("headers", req.headers);
+  req.user = session.user;
+	res.send({
+		user: session,
+	});
+});
+
 app.use(
 	createRequestHandler({
 		build: () => import("virtual:react-router/server-build"),
 		getLoadContext,
 	}),
 );
+
+
